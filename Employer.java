d456acffba683f22e3f883b1da4318abea00f086
Larinayaa <kovalcukanna10@gmail.com>
2024-10-28 14:38:00 +0200

    end

--- Employer.java
+++ Employer.java
@@ -1,6 +1,29 @@
-public class Employer {
+import java.math.BigDecimal;
+
+public class Employer implements Comparable<Employer>{ //реализует интерфейс и
+    // переопределяет метод для сравнения зп
     private int id;
     private  String name;
     private int age;
-    //salary...
+    private BigDecimal salary; //стандартный класс для точного вычисления дробных чисел
+
+    public Employer(int id, String name, int age, BigDecimal salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return  id +
+                " работник. Имя: " + name + '\'' +
+                ". Возраст: " + age +
+                ". Зарплата: " + salary;
+    }
+
+    @Override
+    public int compareTo(Employer o) {
+        return salary.compareTo(o.salary); //для сортировки по возрастанию
+    }
 }
--- Main.java
+++ Main.java
@@ -1,2 +1,27 @@
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+/*
+Hanna Kovalchuk gr 53
+ */
 public class Main {
+    public static void main(String[] args) {
+        Employer[] employers = { //массив работников
+                new Employer(1,"Вася", 28, new BigDecimal("1675.56")),
+                new Employer(2,"Петя", 51, new BigDecimal("5427.22")),
+                new Employer(3,"Анна", 36, new BigDecimal("4434.11")),
+                new Employer(4,"Иван", 44, new BigDecimal("1123.73")),
+                new Employer(5,"Катя", 19, new BigDecimal("8753.12"))
+        };
+        print(employers); //вывод метода принт всех
+        Arrays.sort(employers); //сортировка
+        System.out.println("----------------------------------------");
+        print(employers); //вывод сортировки
+
+    }
+    public static void print(Employer[] employers){
+        for (Employer employer : employers){
+            System.out.println(employer);
+        }
+    }
 }
